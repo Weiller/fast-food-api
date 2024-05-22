2986f0b1927f78d2dd3a98ae855a8c1a9b3a2e4f
Dailan Bueno <daylansantos@gmail.com>
2024-05-21 21:11:02 -0300

    criando endpoint cancelar pedidos

--- src/main/java/br/com/fiap/adapter/repositories/pedido/PedidoEntity.java
+++ src/main/java/br/com/fiap/adapter/repositories/pedido/PedidoEntity.java
@@ -6,8 +6,10 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
@@ -16,6 +18,8 @@ import java.time.LocalDateTime;
 @Table(name = "pedido")
 @Builder
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class PedidoEntity {
     @Id
     @GeneratedValue
